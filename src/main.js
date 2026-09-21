3a54100a928bb99999bd251c09915c757f57eb51
joaquin-aguilar <aguilarjoaquinandre@gmail.com>
2026-09-21 11:51:14 -0500

    feat(components): trash imports

--- src/main.js
+++ src/main.js
@@ -10,10 +10,46 @@ import App from './app.vue'
 import i18n from "./i18n.js";
 import router from "./router.js";
 import pinia from "./pinia.js";
+import {
+    Button,
+    Checkbox,
+    ConfirmationService,
+    ConfirmDialog,
+    Dialog,
+    DialogService, FileUpload, FloatLabel, IconField, InputNumber, InputText, Rating,
+    SelectButton, Textarea,
+    ToastService, Toolbar
+} from "primevue";
 
 const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 createApp(App)
     .use(PrimeVue, {theme:{preset: Material}, ripple: true, license: primeVueUiLicenseKey})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select', Select)
+    .component('pv-select-button', SelectButton)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-text', InputText)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
     .use(i18n)
     .use(router)
     .use(pinia)
