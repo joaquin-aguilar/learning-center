3c3313bc06b964b9d93f16a2de05ed4a9839d07c
joaquin-aguilar <aguilarjoaquinandre@gmail.com>
2026-09-21 12:41:44 -0500

    aa

---
+++ src/shared/presentation/views/not-found.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n();
+</script>
+
+<template>
+  <section class="pt-6 p-4 md:p-5">
+    <div class="flex flex-column gap-3">
+      <h1 class="text-4xl font-bold text-color">{{t('page-not-found.title')}}</h1>
+      <h1 class="text-4xl font-bold text-color">{{t('page-not-found.content')}}</h1>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
