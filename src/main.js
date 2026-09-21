598c2fcbe77da546f9412a2316af45389808db0e
joaquin-aguilar <aguilarjoaquinandre@gmail.com>
2026-09-21 11:05:48 -0500

    feat(i18n): config Vue-i18n library for internationalization support

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import Tooltip from 'primevue/tooltip'
 
 import './style.css'
 import App from './App.vue'
+import i18n from "./i18n.js";
 
 const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 createApp(App)
     .use(PrimeVue, {theme:{preset: Material}, ripple: true, license: primeVueUiLicenseKey})
+    .use(i18n)
     .mount('#app')
