e8ebe732b00cc04600965be90442861a2835280f
joaquin-aguilar <aguilarjoaquinandre@gmail.com>
2026-09-21 11:14:17 -0500

    feat(state-management):  configure Pinia for state management

--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import './style.css'
 import App from './App.vue'
 import i18n from "./i18n.js";
 import router from "./router.js";
+import pinia from "./pinia.js";
 
 const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 createApp(App)
     .use(PrimeVue, {theme:{preset: Material}, ripple: true, license: primeVueUiLicenseKey})
     .use(i18n)
     .use(router)
+    .use(pinia)
     .mount('#app')
