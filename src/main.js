901f022908c8e667f19df4338a94f595c9b1dd01
joaquin-aguilar <aguilarjoaquinandre@gmail.com>
2026-09-21 11:02:15 -0500

    feat(ui):  add primeVue graphic configuration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Material from '@primeuix/themes/material'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import Tooltip from 'primevue/tooltip'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
+createApp(App)
+    .use(PrimeVue, {theme:{preset: Material}, ripple: true, license: primeVueUiLicenseKey})
+    .mount('#app')
